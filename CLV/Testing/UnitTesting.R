4ad0dd2ff30d919fcb3c16a8c9782bcef577d28d
Bryse Wagner <bryse1225@gmail.com>
2022-02-19 17:04:05 -0500

    Added starting testing suite

---
+++ CLV/Testing/UnitTesting.R
@@ -0,0 +1,8 @@
+
+# make sure that testthat is installed before running install.packages("testthat")
+library("testthat")
+
+# example of setup can be found at https://www.johndcook.com/blog/2013/06/12/example-of-unit-testing-r-code-with-testthat/
+
+# give testthat the name of the file being tested
+#source(")
