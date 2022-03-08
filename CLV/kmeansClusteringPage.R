b699b0ddd6ce1a515b74d37971618b97e7d06696
Ben Barasch <baraschben@gmail.com>
2022-03-08 15:58:27 -0500

    Added sliders

--- CLV/kmeansClusteringPage.R
+++ CLV/kmeansClusteringPage.R
@@ -9,6 +9,14 @@ kmeansClusteringPage <- function() {
       column(6,
              plotOutput("kmeans_cluster_plot")
       )
+    ),
+    fluidRow(
+      sliderInput("kmeans_cluster_num",
+                  label = "Number of Clusters",
+                  min = 1,
+                  max = 20,
+                  value = 4,
+                  width = "100%")
     )
   )
 }
--- CLV/pamClusteringPage.R
+++ CLV/pamClusteringPage.R
@@ -3,6 +3,14 @@ pam_clustering_page <- function() {
     titlePanel("PAM Clustering"),
     fluidRow(
       plotOutput("pam_cluster_plot")
+    ),
+    fluidRow(
+      sliderInput("pam_cluster_num",
+                  label = "Number of Clusters",
+                  min = 1,
+                  max = 20,
+                  value = 4,
+                  width = "100%")
     )
   )
 }
